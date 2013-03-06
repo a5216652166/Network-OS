748efe5cf412aa2aa438d12c50325e26c6096431
Joker Hacker <jokerhacker03@gmail.com>
2013-03-06 19:57:28 +0530

    Added process start and stop

--- fs/networkOS-init.sh
+++ fs/networkOS-init.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
+echo "##############################"
+echo "        Network OS 0.1        "
+echo "##############################"
+echo "                            "
 
+echo "-> NetworkOS ... Cleaning up"
 
-echo "NetworkOS .... Initializing ....."
+echo "-> Please wait ......"
+
+RESULT=`ps -a | sed -n /nosMgr/p`
+
+if [ "${RESULT:-null}" = null ]; then
+	sleep 1
+else
+	pkill -TERM nosMgr
+
+	while [ -f  /opt/NetworkOS/NwtMgrDone ];
+	do
+		sleep 1
+	done
+fi
+
+echo "-> NetworkOS .... Initializing ....."
+
+useradd -groot -d/home/cli -ccli_created -s/opt/NetworkOS/sbin/cli cli &>/dev/null
 
 mkdir -p /opt/NetworkOS/etc/
 touch /opt/NetworkOS/etc/ospfd.conf
@@ -15,9 +37,9 @@ do
 	sleep 1
 done
 
-echo "NetworkOS .... Initialized"
+echo "-> NetworkOS .... Initialized"
 
-echo "Starting CLI session"
+#echo "-> Starting CLI session"
 sleep 1
 
-/opt/NetworkOS/sbin/cli
+#/opt/NetworkOS/sbin/cli
